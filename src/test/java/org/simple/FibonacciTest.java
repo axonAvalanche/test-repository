b315997a58db73956a7648a3167634a17abd946a
axonAvalanche <axonAvalanche@outlook.com>
2024-03-05 21:31:55 +0100

    Add dummy test

---
+++ src/test/java/org/simple/FibonacciTest.java
@@ -0,0 +1,14 @@
+package org.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FibonacciTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
