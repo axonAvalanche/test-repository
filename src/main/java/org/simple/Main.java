6b8929b47a5603d733dbc20fd2428380f22fd8a6
axonAvalanche <axonAvalanche@outlook.com>
2024-03-11 01:29:28 +0100

    Time tracker

--- src/main/java/org/simple/Main.java
+++ src/main/java/org/simple/Main.java
@@ -6,19 +6,7 @@ import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            out.println("Please provide a position as a command-line argument.");
-            return;
-        }
-
-        try {
-            int position = Integer.parseInt(args[0]);
-            var fizzBuzzOutput = FizzBuzz.fizzBuzz(position);
-            out.println(
-                    MessageFormat.format("Fizzbuzz output: ", fizzBuzzOutput)
-            );
-        } catch (NumberFormatException e) {
-            out.println("Please provide a valid integer number.");
-        }
+        var service = new UserManagementService();
+        service.registerUser("user", "user@example.com", "Password123@");
     }
 }
---
+++ src/main/java/org/simple/UserManagementService.java
@@ -0,0 +1,51 @@
+package org.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserManagementService {
+    private List<User> users = new ArrayList<>();
+
+    public boolean registerUser(String username, String email, String password) {
+        if (!isValidUsername(username) || !isValidPassword(password)) {
+            return false;
+        }
+
+        for (User user : users) {
+            if (user.getEmail().equalsIgnoreCase(email)) {
+                System.out.println("A user with this email already exists.");
+                return false;
+            }
+        }
+
+        User newUser = new User(username, email, password);
+        users.add(newUser);
+        return true;
+    }
+
+    private boolean isValidUsername(String username) {
+        String usernamePattern = "^[a-zA-Z0-9_-]{2,10}$";
+        return username.matches(usernamePattern);
+    }
+
+    private boolean isValidPassword(String password) {
+        String passwordPattern = "^(?=.*[0-8])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        return password.matches(passwordPattern);
+    }
+
+    public void resetPassword(String email, String newPassword) {
+        for (User user : users) {
+            if (user.getEmail().equalsIgnoreCase(email)) {
+                if (isValidPassword(newPassword)) {
+                    user.setPassword(newPassword);
+                    System.out.println("Password successfully reset.");
+                } else {
+                    System.out.println("New password does not meet the requirements.");
+                }
+                return;
+            }
+        }
+
+        System.out.println("User with email " + email + " not found.");
+    }
+}
